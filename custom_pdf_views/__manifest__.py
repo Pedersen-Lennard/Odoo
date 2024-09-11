2829a92ac6c1bc7c7f73b61f016f5f1812c4bb14
IDCOLL <ian007collins@hotmail.com>
2024-09-11 15:50:52 +0200

    Add files via upload

--- custom_pdf_views/__manifest__.py
+++ custom_pdf_views/__manifest__.py
@@ -4,11 +4,18 @@
 {
     'name': 'Custom PDF Views',
     'summary': 'These are custom views for the pdf invoices and quotations',
-    'depends': ['base', 'account', 'sale', 'product', 'purchase', 'stock'],
+    'depends': ['base', 'account', 'sale', 'product', 'purchase', 'stock', 'mrp_workorder', 'web'],
+    'assets': {
+        'web.assets_backend': [
+            'custom_pdf_views/static/src/components/*/*.js',
+            'custom_pdf_views/static/src/components/*/*.xml',
+            # 'custom_pdf_views/static/src/components/*/*.scss',
+        ]
+    },
     'data': [
         'views/custom_address_template.xml',
         'views/custom_invoice.xml',
         'views/custom_quotations.xml',
-        'views/custom_template.xml',
+        'views/custom_template.xml'
     ],
 }
